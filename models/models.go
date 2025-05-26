1125ad1b127cfb6339b869c6002c04d5a7b89007
pereira299 <lucasper057@gmail.com>
2025-05-26 01:27:30 -0300

    feat(models): update role constants and enhance Booking struct with additional fields

--- models/models.go
+++ models/models.go
@@ -8,18 +8,23 @@ import (
 
 const (
 	President uint = 1
-	Vice           = 2
-	Secretary      = 3
-	Treasurer      = 4
-	Director       = 5
+	Vice_tsi       = 2
+	Vice_comp 	   = 3
+	Secretary      = 4
+	Treasurer      = 5
+	Director       = 6
+	Other 		   = 7
 )
 
 const (
-	COMP  uint = 1
+	COMP  uint = 1 
 	TSI        = 2
 	ELET       = 3
 	CIVIL      = 4
 	EBB        = 5
+	TPQ		   = 6
+	MAT 	   = 7
+	OTHER	   = 8
 )
 
 type User struct {
@@ -88,7 +93,10 @@ type Booking struct {
 	TimestampEnd   time.Time `json:"time-end"`
 	Description    string    `json:"description"`
 
-	Day string
+	Day 		string
+	Title 		string 	`json:"title"`
+	Location 	string 	`json:"location"`
+	Private 	bool 	`json:"private"`
 
 	Username string `json:"username"`
 	Role     uint   `json:"role"`
